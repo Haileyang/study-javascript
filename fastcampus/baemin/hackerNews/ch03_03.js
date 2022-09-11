ee084aac7b52cc2970ed5f0ad5aa42a698a59815
hailey <yanghyeri73@gmail.com>
2022-09-10 23:58:07 -0700

    김민태 1강 - Kit_Hacker News Client : Ch03_03 - 두 개의 화면을 가진 웹앱

--- fastcampus/baemin/hackerNews/ch03_03.js
+++ fastcampus/baemin/hackerNews/ch03_03.js
@@ -1,3 +1,10 @@
+//두개의 화면을 가진 웹앱
+
+//학습목표 
+// 1. anchor tag 를 li 태그 안에 삽입
+// 2. event listener = hashchange 
+// 3. content 공간 확보 후, title 출력
+
 const ajax = new XMLHttpRequest();  
 const content = document.createElement('div')
 const root = document.getElementById('root')
@@ -13,25 +20,32 @@ const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul')
 
 window.addEventListener('hashchange', function(){
-    
-    const id = location.hash.substr(1)
+    // console.log('hash change')
 
+    // location.hash 
+    // console.log(location.hash) //해쉬태그까지 함께 반환 
+    //해쉬태그 제거를 위해서는 substr을 사용 
+    const id = location.hash.substr(1)
+    console.log(id)
+    
     ajax.open('GET', CONTENT_URL.replace('@id', id), false)
     ajax.send(); 
 
     const newsContent = JSON.parse(ajax.response);
     const title = document.createElement('hi')
-
     content.appendChild(title)
     title.innerHTML = newsContent.title
+    
+    console.log(newsContent)
 })
 
 for(let i=0; i<10; i++){
     const li = document.createElement('li')
     const a = document.createElement('a')
-
     a.innerHTML = `${newsFeed[i].title}(${newsFeed[i].comments_count})`
+    // a.href = '#'
     a.href = `#${newsFeed[i].id}` 
+    // a.addEventListener('click', function(){}) //앵커 태그가 여러개 있으므로 각 앵커태그에 부여해야되는 불편함이 있음.
 
     li.appendChild(a)
     ul.appendChild(li)
